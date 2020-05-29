8b4976ece2f77df36b9077c8aa5fc3baa490a815
Andry <gaxeyat298@chordmi.com>
2020-05-29 11:27:52 +0300

    Updates

---
+++ precache-manifest.9406eb0d8ecac5b866ef33ec3c739a8d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274b2957015558a3a84dacdc188a7201",
+    "url": "/pushV/index.html"
+  },
+  {
+    "revision": "f9b78f175c1419617756",
+    "url": "/pushV/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "ffc34e4a1769a8bab560",
+    "url": "/pushV/static/css/main.103fd6a1.chunk.css"
+  },
+  {
+    "revision": "f9b78f175c1419617756",
+    "url": "/pushV/static/js/2.c5bd9d8a.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/pushV/static/js/2.c5bd9d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc34e4a1769a8bab560",
+    "url": "/pushV/static/js/main.3282d9e7.chunk.js"
+  },
+  {
+    "revision": "19e274a083ddc594264c",
+    "url": "/pushV/static/js/runtime-main.03a0eed5.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/pushV/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "36ec06394b9c159b44e3384ae610d353",
+    "url": "/pushV/static/media/yGmxFwe.36ec0639.png"
+  }
+]);
